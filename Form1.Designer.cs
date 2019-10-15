188a7bc9b117dd723646cf9c885ff7f2855cb2c9
Yakov Budnikov <dizertir64@gmail.com>
2019-10-15 15:24:17 +0300

    new add

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace VijinerTableSymbl_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.CodeTB = new System.Windows.Forms.RichTextBox();
+            this.DecodeTB = new System.Windows.Forms.RichTextBox();
+            this.TextLable = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.KeyTB = new System.Windows.Forms.TextBox();
+            this.CodeButton = new System.Windows.Forms.Button();
+            this.DecodeButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 31);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.файлToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(62, 27);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(216, 28);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.ОткрытьToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // CodeTB
+            // 
+            this.CodeTB.Location = new System.Drawing.Point(518, 75);
+            this.CodeTB.Name = "CodeTB";
+            this.CodeTB.Size = new System.Drawing.Size(270, 363);
+            this.CodeTB.TabIndex = 1;
+            this.CodeTB.Text = "";
+            // 
+            // DecodeTB
+            // 
+            this.DecodeTB.Location = new System.Drawing.Point(12, 75);
+            this.DecodeTB.Name = "DecodeTB";
+            this.DecodeTB.Size = new System.Drawing.Size(270, 363);
+            this.DecodeTB.TabIndex = 2;
+            this.DecodeTB.Text = "";
+            // 
+            // TextLable
+            // 
+            this.TextLable.AutoSize = true;
+            this.TextLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.TextLable.Location = new System.Drawing.Point(12, 49);
+            this.TextLable.Name = "TextLable";
+            this.TextLable.Size = new System.Drawing.Size(128, 20);
+            this.TextLable.TabIndex = 3;
+            this.TextLable.Text = "Расшифровка";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.label2.Location = new System.Drawing.Point(514, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Шифровка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.label3.Location = new System.Drawing.Point(285, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ключ";
+            // 
+            // KeyTB
+            // 
+            this.KeyTB.Location = new System.Drawing.Point(289, 75);
+            this.KeyTB.Name = "KeyTB";
+            this.KeyTB.Size = new System.Drawing.Size(223, 22);
+            this.KeyTB.TabIndex = 6;
+            // 
+            // CodeButton
+            // 
+            this.CodeButton.Location = new System.Drawing.Point(288, 389);
+            this.CodeButton.Name = "CodeButton";
+            this.CodeButton.Size = new System.Drawing.Size(108, 49);
+            this.CodeButton.TabIndex = 7;
+            this.CodeButton.Text = "Шифровка";
+            this.CodeButton.UseVisualStyleBackColor = true;
+            this.CodeButton.Click += new System.EventHandler(this.CodeButton_Click);
+            // 
+            // DecodeButton
+            // 
+            this.DecodeButton.Location = new System.Drawing.Point(404, 389);
+            this.DecodeButton.Name = "DecodeButton";
+            this.DecodeButton.Size = new System.Drawing.Size(108, 49);
+            this.DecodeButton.TabIndex = 8;
+            this.DecodeButton.Text = "Дешифровка";
+            this.DecodeButton.UseVisualStyleBackColor = true;
+            this.DecodeButton.Click += new System.EventHandler(this.DecodeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DecodeButton);
+            this.Controls.Add(this.CodeButton);
+            this.Controls.Add(this.KeyTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextLable);
+            this.Controls.Add(this.DecodeTB);
+            this.Controls.Add(this.CodeTB);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox CodeTB;
+        private System.Windows.Forms.RichTextBox DecodeTB;
+        private System.Windows.Forms.Label TextLable;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox KeyTB;
+        private System.Windows.Forms.Button CodeButton;
+        private System.Windows.Forms.Button DecodeButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VijinerTableSymbl_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ОткрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            string FName = openFileDialog1.FileName;
+
+            CodeTB.Text = "";
+            DecodeTB.Text = "";
+            
+            int LengthF = System.IO.File.ReadAllLines(FName).Length;
+            for (int i = 0; i < LengthF; i++)
+            {
+                string temp = System.IO.File.ReadLines(FName).Skip(i).FirstOrDefault();
+                if (FName[FName.Length - 5] == 'd')
+                {
+                    DecodeTB.Text += temp;
+                }
+                else if (FName[FName.Length - 5] == 'c')
+                {
+                    CodeTB.Text += temp;
+                }
+                else return;
+            }
+        }
+
+        private void CodeButton_Click(object sender, EventArgs e)
+        {
+            string DecodeText = DecodeTB.Text;
+            int DecodeTextLength = DecodeText.Length;
+
+            string KeyText = KeyTB.Text;
+            int n = KeyTB.Text.Length;
+            
+            if (DecodeTextLength % n != 0)
+            {
+                int addSpace = n - DecodeTextLength % n;
+                for(int i = 0; i < addSpace; ++i)
+                {
+                    DecodeTB.Text += ' ';
+                }
+            }
+
+            for (int i = 0; i < DecodeTextLength - 1; i += n)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int NewChar = (Convert.ToInt32(DecodeText[i + j]) + Convert.ToInt32(KeyText[j])) % 65536;
+                    CodeTB.Text += Convert.ToChar(NewChar);
+                }
+            }
+        }
+
+        private void DecodeButton_Click(object sender, EventArgs e)
+        {
+            string CodeText = CodeTB.Text;
+            int CodeTextLength = CodeText.Length;
+
+            string KeyText = KeyTB.Text;
+            int n = KeyTB.Text.Length;
+
+            if (CodeTextLength % n != 0)
+            {
+                int addSpace = n - CodeTextLength % n;
+                for (int i = 0; i < addSpace; ++i)
+                {
+                    CodeTB.Text += ' ';
+                }
+            }
+
+            for (int i = 0; i < CodeTextLength - 1; i += n)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int NewChar = (Convert.ToInt32(CodeText[i + j]) - Convert.ToInt32(KeyText[j]));
+                    if (NewChar < 0) NewChar += 65536;
+                    DecodeTB.Text += Convert.ToChar(NewChar);
+                }
+            }
+        }
+    }
+}
